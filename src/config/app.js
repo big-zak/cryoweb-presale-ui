512fe966d96f90fa836999a3e718529ede161308
Zak Pie <zakpie@protonmail.com>
2021-07-04 08:27:25 +0000

    Update app.js

--- src/config/app.js
+++ src/config/app.js
@@ -18,7 +18,7 @@ export default {
     defaultReferralAddres: "0x0d7395E4fad66a6f56B62b3318185647768eA5F7",
 
     // contract address
-    contractAddress: "0x8B246d031485F24F3081A8C72f6A304930D0f3F1",
+    contractAddress: "0xd5E653B1bFc4284A217fD12e631b0B0F8C6a910f",
 
     //supportedChainId: 0x38 // 56 is Binance Smart chain
 
